f1103813463c3f86277b9cf45b0ca3b0125e588e
mumbi58 <reggiekariuki58@gmail.com>
2024-06-12 14:50:46 +0300

    hdsghs

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Dashboard from './Components/Dashboard/dashboard'
+
+export default function App() {
+  return (
+    <div>App
+      <Dashboard />
+    </div>
+  )
+}
---
+++ src/Components/Dashboard/dashboard.jsx
@@ -0,0 +1,13 @@
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+
+export default function Dashboard() {
+  return (
+  <Flex>
+    <Box w="150px" h="50px" bg="blue">1</Box>
+    <Box w="150px" h="50px" bg="black">2</Box>
+    <Box w="150px" h="50px" bg="green">3</Box>
+    <Box w="150px" h="50px" bg="yellow">4</Box>
+  </Flex>
+
+  )
+}
