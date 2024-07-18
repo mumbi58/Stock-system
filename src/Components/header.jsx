60fc723a55d9946fe71e34798a57441687e4716c
mumbi58 <reggiekariuki58@gmail.com>
2024-07-18 10:16:31 +0300

    added user profile

---
+++ src/Components/header.jsx
@@ -0,0 +1,23 @@
+import { Flex, Box, Text, IconButton, Avatar, Menu, MenuButton, MenuList, MenuItem, MenuDivider, Heading } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
+
+
+export const Header = ({ onLogout }) => {
+    const navigate = useNavigate();
+
+    return (
+        <Flex justifyContent="space-between" alignItems="center" p={0.1} bg="gray.200" paddingRight={9} >
+            <Box>
+            </Box>
+            <Menu>
+                <MenuButton as={Flex} alignItems="center" cursor="pointer">
+                    <IconButton icon={<FaUser />} variant="link" aria-label="Profile" />
+                </MenuButton>            <MenuList>
+                    <MenuItem onClick={() => navigate('/profile')}>Profile</MenuItem>
+                    <MenuDivider />
+                </MenuList>
+            </Menu>
+        </Flex>
+    );
+};
---
+++ src/Components/userprofile.jsx
@@ -0,0 +1,20 @@
+import { Box, Heading, Text, Button } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+export default function Userprofile() {
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate('/');
+  };
+  return (
+    <Box maxW="md" mx="auto" mt={8} p={6} boxShadow="md" borderRadius="md">
+      <Heading mb={2} textAlign="center">User Profile</Heading>
+      <Text mb={4}>Username: JohnDoe</Text>
+      <Text mb={4}>Email: johndoe@example.com</Text>
+      <Button colorScheme="blue" width="full" mt={4} onClick={handleLogout}>
+        Logout
+      </Button>
+    </Box>
+  );
+}
